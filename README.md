6dd535c7d98af0acbcf35d7ee3a6af4762d340b0
ashlen-renner <ashlenrenner@gmail.com>
2016-04-15 15:45:40 -0400

    styling

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # j583-project3
+
+To open the project, go into the command line and find the project on your computer. Then type serve -p 3000 to open the server. Type localhost: 3000 into the browser. 
--- js/app.js
+++ js/app.js
@@ -169,39 +169,40 @@ var margin = {top: 20, right: 100, bottom: 30, left: 100},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
             // Parse the date / time
-            var parseYear = d3.time.format("%Y").parse;
+var parseYear = d3.time.format("%Y").parse;
 
-            // Set the ranges
-            var x = d3.time.scale().range([0, width]);
-            var y = d3.scale.linear().range([height, 0]);
+// Set the ranges
+var x = d3.time.scale().range([0, width]);
+var y = d3.scale.linear().range([height, 0]);
 
-            // Define the axes
-            var xAxis = d3.svg.axis().scale(x)
-                .orient("bottom").ticks(5);
+// Define the axes
+var xAxis = d3.svg.axis().scale(x)
+    .orient("bottom").ticks(5);
 
-            var yAxis = d3.svg.axis().scale(y)
-                .orient("left").ticks(5)
-                .tickFormat( function(d) { return "$" + d } );
+var yAxis = d3.svg.axis().scale(y)
+    .orient("left").ticks(5)
+    .tickFormat( function(d) { return "$" + d } );
 
-            // Define the line
-            var valueline = d3.svg.line()
-                .x(function(d) { return x(d.year); })
-                .y(function(d) { return y(d.budget); });
+// Define the line
+var valueline = d3.svg.line()
+    .x(function(d) { return x(d.year); })
+    .y(function(d) { return y(d.budget); });
 
-            // Adds the svg canvas
-            var svg = d3.select("body")
-                .append("svg")
-                    .attr("width", width + margin.left + margin.right)
-                    .attr("height", height + margin.top + margin.bottom)
+// Adds the svg canvas
+var svg = d3.select("body")
+    .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
 
-                .append("g")
-                    .attr("transform",
-                          "translate(" + margin.left + "," + margin.top + ")");
+    .append("g")
+        .attr("transform",
+              "translate(" + margin.left + "," + margin.top + ")");
 
-var color = d3.scale.category20();
-          // color array
-          var bluescale4 = ["#FFAF2B"];
-          var color = d3.scale.ordinal().range(bluescale4);
+
+// var color = d3.scale.category20();
+//           // color array
+//           var bluescale4 = ["#FFAF2B"];
+//           var color = d3.scale.ordinal().range(bluescale4);
 
             // Get the data
             d3.csv("budget.csv", function(error, data) {
@@ -217,6 +218,8 @@ var color = d3.scale.category20();
                 // Add the valueline path.
                 svg.append("path")
                     .attr("class", "line")
+                    .attr("stroke", "red")
+                    .attr("stroke-width", 2)
                     .attr("d", valueline(data));
 
                 // Add the X Axis
